aeb25bf5283410f3de2cde9d87d1003af9610121
tariqbroadnax <tariqbroadnax@gmail.com>
2022-10-10 09:57:45 -0400

    bb-82: added log statements to the aws event handlers.

--- aws/refresh.handler.ts
+++ aws/refresh.handler.ts
@@ -1,3 +1,7 @@
+import createLogger from '../logging/logger';
+const logger = createLogger('aws/refresh.handler.ts');
+
 exports.handler = () => {
+  logger.info('handling aws event in refresh');
   require('../commands/refresh');
 };
--- aws/scrape.handler.ts
+++ aws/scrape.handler.ts
@@ -1,3 +1,7 @@
+import createLogger from '../logging/logger';
+const logger = createLogger('aws/scrape.handler.ts');
+
 exports.handler = () => {
+  logger.info('handling aws event in scrape');
   require('../commands/scrape');
 };
